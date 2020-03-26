cf1644572a07f766d3c90ca88e6a24c4cc58e3f5
Amirtha <amirtha4501@gmail.com>
2020-03-26 12:04:50 +0530

    In-app Navigation

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { Hero } from '../hero';
+import { HeroService }  from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -8,11 +12,30 @@ import { Hero } from '../hero';
 })
 export class HeroDetailComponent implements OnInit {
 
-  @Input() hero: Hero;
+  hero: Hero;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute, //holds information about the route to this instance of the HeroDetailComponent. This component is interested in the route's parameters extracted from the URL.
+    private heroService: HeroService, //gets hero data from the remote server and this component will use it to get the hero-to-display.
+    private location: Location //is an Angular service for interacting with the browser.
+  ) { }
 
   ngOnInit(): void {
+    this.getHero();
   }
 
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    // .route.snapshot is a static image of the route information shortly after the component was created.
+    // paramMap is a dictionary of route parameter values extracted from the URL. The "id" key returns the id of the hero to fetch.
+    // Route parameters are always strings. The JavaScript (+) operator converts the string to a number, which is what a hero id should be.
+    console.log(id);
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+  
 }
